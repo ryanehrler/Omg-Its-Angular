670f3762b45bb086a1bda2b05dc36ed5db6981e3
Ryan Ehrler <rehrler@global-indemnity.com>
2019-01-25 15:40:36 -0500

    Added more content to angular-building-blocks

--- src/app/features/intro-to-angular/angular-building-blocks/angular-building-blocks.component.ts
+++ src/app/features/intro-to-angular/angular-building-blocks/angular-building-blocks.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  Input,
+  OnInit
+  } from '@angular/core';
 import { SyntaxStyle } from 'ngx-syntax-highlighter';
 
 @Component({
@@ -8,21 +13,6 @@ import { SyntaxStyle } from 'ngx-syntax-highlighter';
 })
 export class AngularBuildingBlocksComponent implements OnInit {
   styles = SyntaxStyle;
-  typescript = `class MyClass {
-    public static myValue: string;
-    constructor(init: string) {
-      this.myValue = init;
-    }
-  }
-  import fs = require("fs");
-  module MyModule {
-    export interface MyInterface extends Other {
-      myProperty: any;
-    }
-  }
-  declare magicNumber number;
-  myArray.forEach(() => { }); // fat arrow syntax
-  `;
 
   constructor() {}
 
